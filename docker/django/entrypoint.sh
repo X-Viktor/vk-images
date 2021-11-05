2b30f1f1f91faffb1fca40da5f2502115da4c5b2
Viktor X <x.viktor.13@gmail.com>
2021-11-05 14:20:52 +0700

    Add .env

--- docker/django/entrypoint.sh
+++ docker/django/entrypoint.sh
@@ -6,4 +6,4 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
-gunicorn config.wsgi:application -b 0.0.0.0:8000 --reload
+exec gunicorn config.wsgi:application -b 0.0.0.0:$PORT --reload
